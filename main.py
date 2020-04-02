0e2245cb10ae0001aba761809a984e88c28ef091
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2020-04-02 16:34:39 +0300

    eliminate sorting & simplify multiprocessing

--- main.py
+++ main.py
@@ -24,38 +24,44 @@ def valid_k_gram(k_gram, edges):
                and not any({*k_gram} == {e} for e in edges) \
                if L <= R)
 
-def mtslia(data, k=2, edges=('>', '<')):
+def map_k_gram_to_tier(k_gram, data, edges):
+    k = len(k_gram)
+    projection = []
+    for m, datum in enumerate(data):
+        projection += [(m, -(i + 1), edges[0]) for i in range(k)]
+        for n, segment in enumerate(datum):
+            if segment in k_gram:
+                projection += [(m, n, segment)]
+        projection += [(m, len(datum) + i, edges[1]) for i in range(k)]
+    blocker_sets = set()
+    for offset in range(len(projection) - k + 1):
+        M, N, segments = zip(*projection[offset:(offset + k)])
+        if segments == k_gram and all(m == M[0] for m in M):
+            blocker_set = set()
+            for n1, n2 in zip(N[:-1], N[1:]):
+                blocker_set |= set(data[M[0]][(n1 + 1):n2])
+            blocker_sets.add(tuple(sorted(blocker_set)))
+    blockers = set(blocker for blocker_set in blocker_sets for blocker in blocker_set)
+    if blocker_sets:
+        if () in blocker_sets:
+            return k_gram, None
+        for blocker in sorted(blockers, reverse=True):
+            if all(blockers - {blocker} & set(blocker_set) for blocker_set in blocker_sets):
+                blockers -= {blocker}
+    tier = tuple(sorted({*k_gram, *blockers} - {*edges}))    
+    return k_gram, tier
+
+def learn(data, k=2, runner=itertools.starmap, edges=('>', '<')):
     alpha = extract_alphabet(data)
     attested = extract_local_k_grams(data, k, edges)
     grammar = collections.defaultdict(set)
-    for k_gram in itertools.product(alpha | {*edges}, repeat=k):
-        if k_gram in attested:
-            continue
-        if not valid_k_gram(k_gram, edges):
-            continue
-        projection = []
-        for m, datum in enumerate(data):
-            projection += [(m, -(i + 1), edges[0]) for i in range(k)]
-            for n, segment in enumerate(datum):
-                if segment in k_gram:
-                    projection += [(m, n, segment)]
-            projection += [(m, len(datum) + i, edges[1]) for i in range(k)]
-        projection.sort()
-        blocker_sets = set()
-        for offset in range(len(projection) - k + 1):
-            M, N, segments = zip(*projection[offset:(offset + k)])
-            if segments == k_gram and all(m == M[0] for m in M):
-                blocker_set = set()
-                for n1, n2 in zip(N[:-1], N[1:]):
-                    blocker_set |= set(data[M[0]][(n1 + 1):n2])
-                blocker_sets.add(tuple(sorted(blocker_set)))
-        blockers = set(blocker for blocker_set in blocker_sets for blocker in blocker_set)
-        if blocker_sets:
-            if () in blocker_sets:
-                continue
-            for blocker in sorted(blockers, reverse=True):
-                if all(blockers - {blocker} & set(blocker_set) for blocker_set in blocker_sets):
-                    blockers -= {blocker}
-        tier = tuple(sorted({*k_gram, *blockers} - {*edges}))
-        grammar[tier].add(k_gram)
+    k_grams = itertools.product(alpha | {*edges}, repeat=k)
+    runner_result = runner(
+        map_k_gram_to_tier,
+        ((k_gram, data, edges) for k_gram in k_grams \
+         if valid_k_gram(k_gram, edges) and k_gram not in attested)
+    )
+    for k_gram, tier in runner_result:
+        if tier is not None:
+            grammar[tier].add(k_gram)    
     return dict(grammar)
