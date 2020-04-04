c7c6b19938bb5cbc059032f5e8316de8ee429b1e
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2020-04-04 18:39:53 +0300

    overlap support, take 2

--- k_mtslia.py
+++ k_mtslia.py
@@ -24,7 +24,7 @@ def valid_k_gram(k_gram, edges):
                and not any({*k_gram} == {e} for e in edges) \
                if L <= R)
 
-def map_k_gram_to_tier(k_gram, data, edges):
+def map_k_gram_to_blockers(k_gram, data, edges):
     k = len(k_gram)
     projection = []
     for m, datum in enumerate(data):
@@ -33,35 +33,48 @@ def map_k_gram_to_tier(k_gram, data, edges):
             if segment in k_gram:
                 projection += [(m, n, segment)]
         projection += [(m, len(datum), edges[1]) for i in range(k - 1)]
-    blocker_sets = set()
+    blocker_cnf = set()
     for offset in range(len(projection) - k + 1):
         M, N, segments = zip(*projection[offset:(offset + k)])
         if segments == k_gram and all(m == M[0] for m in M):
-            blocker_set = set()
+            blocker_clause = set()
             for n1, n2 in zip(N[:-1], N[1:]):
-                blocker_set |= set(data[M[0]][(n1 + 1):n2])
-            blocker_sets.add(tuple(sorted(blocker_set)))
-    blockers = set(blocker for blocker_set in blocker_sets for blocker in blocker_set)
-    if blocker_sets:
-        if () in blocker_sets:
-            return k_gram, None
-        for blocker in sorted(blockers, reverse=True):
-            if all(blockers - {blocker} & set(blocker_set) for blocker_set in blocker_sets):
-                blockers -= {blocker}
-    tier = tuple(sorted({*k_gram, *blockers} - {*edges}))    
-    return k_gram, tier
+                blocker_clause |= set(data[M[0]][(n1 + 1):n2])
+            blocker_cnf.add(tuple(sorted(blocker_clause)))
+    return k_gram, tuple(sorted(set(blocker_cnf)))
 
-def learn(data, k=2, runner=itertools.starmap, edges=('>', '<')):
+def extract_tier_no_overlap(blocker_cnf, k_gram, edges):
+    blockers = set(blocker for blocker_clause in blocker_cnf for blocker in blocker_clause)
+    if blocker_cnf:
+        for blocker in sorted(blockers, reverse=True):
+            if all(blockers - {blocker} & set(blocker_clause) for blocker_clause in blocker_cnf):
+                blockers -= {blocker}
+    return tuple((element,) for element in sorted({*blockers} - {*edges}))
+
+def learn(data, k=2, overlap=True, edges=('>', '<'), runner=itertools.starmap):
     alpha = extract_alphabet(data)
     attested = extract_local_k_grams(data, k, edges)
     grammar = collections.defaultdict(set)
     k_grams = itertools.product(alpha | {*edges}, repeat=k)
     runner_result = runner(
-        map_k_gram_to_tier,
+        map_k_gram_to_blockers,
         ((k_gram, data, edges) for k_gram in k_grams \
          if valid_k_gram(k_gram, edges) and k_gram not in attested)
     )
-    for k_gram, tier in runner_result:
-        if tier is not None:
-            grammar[tier].add(k_gram)    
-    return dict(grammar)
+    grammar = ()
+    for k_gram, blocker_cnf in runner_result:
+        if () in blocker_cnf:
+            continue
+        if overlap:
+            grammar += ((k_gram, blocker_cnf),)
+        else:
+            grammar += ((k_gram, extract_tier_no_overlap(blocker_cnf, k_gram, edges)),)
+    return grammar
+
+def scan(string, grammar, edges=('>', '<')):
+    for k_gram, blocker_cnf in grammar:
+        k_gram, scan_cnf = map_k_gram_to_blockers(k_gram, (string,), edges)
+        for scan_clause in scan_cnf:
+            if all(not {*blocker_clause}.issubset({*scan_clause}) for blocker_clause in blocker_cnf):
+                return False
+    return True
